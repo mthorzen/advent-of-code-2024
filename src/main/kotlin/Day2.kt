21ce7c81f51955f1d66abca3d116f42bd10fc0f2
Michael Thorzén <michael.thorzen@nordoba.com>
2024-12-02 11:26:29 +0100

    Day 2 part 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,31 +1,34 @@
 package com.nordoba
 
 import java.io.File
-import kotlin.math.abs
 
 fun main() {
     println("Day 2!")
 
     var safeReports = 0
     var unsafeReports = 0
+    val problemDampener = true
+
+    // part 1: 383
+    // part 2 first try: safe: 564, unsafe: 436 (too high)
+    // part 2 second try: safe: 229 unsafe: 771 (too low)
+    // part 2 third try: safe: 436 unsafe: 564 (correct)
 
     File("src/main/resources/day2-input.txt").forEachLine { line ->
         val levels = line.split("\\s+".toRegex()).map { it.toInt() }
-        val differences = levels.zipWithNext().map { (a, b) -> a - b }
-        println(levels + " - " + differences)
-        if (differences.all { it >= 0 } || differences.all { it <= 0 }) {
-            if (differences.all { abs(it) in 1..3 }) {
-                println("safe")
-                safeReports++
-            } else {
-                println("unsafe1")
-                unsafeReports++
-            }
+        var validSerie = levels.zipWithNext().all { (a, b) -> b > a && b - a in 1..3 } || levels.zipWithNext().all { (a, b) -> a > b && a - b in 1..3 }
+        if (problemDampener && !validSerie) {
+            validSerie = levels.indices.any { index ->
+                val modifiedLevels = levels.toMutableList().apply { removeAt(index) }
+                modifiedLevels.zipWithNext().all { (a, b) -> b > a && b - a in 1..3 } || modifiedLevels.zipWithNext().all { (a, b) -> a > b && a - b in 1..3 } }
+        }
+
+        if (validSerie) {
+            safeReports++
         } else {
-            println("unsafe2")
             unsafeReports++
         }
     }
 
-    println("Part 1 safe: $safeReports unsafe: $unsafeReports")
+    println("safe: $safeReports unsafe: $unsafeReports")
 }
